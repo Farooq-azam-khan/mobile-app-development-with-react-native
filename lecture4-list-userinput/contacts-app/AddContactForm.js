b9cef7023060de13d471eceb71c0f2d73ac1ca6e
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2019-07-02 21:24:54 -0400

    started form input

---
+++ lecture4-list-userinput/contacts-app/AddContactForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, TextInput, View } from 'react-native';
+
+export default class AddContactForm extends React.Component {
+    render() {
+        <View>
+            <TextInput />
+            <TextInput />
+            <Button title="Add Contact" />
+        </View>
+    }
+}
