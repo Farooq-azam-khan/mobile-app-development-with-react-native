1521ef02f771c5f0561d5f6b718281f2ac32182d
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2019-07-02 20:11:52 -0400

    added styles for row.js

--- lecture4-list-userinput/contacts-app/Row.js
+++ lecture4-list-userinput/contacts-app/Row.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 const Row = props => (
     // <View key={props.key}>
-    <View>
+    <View style={styles.row}>
         <Text>{props.name}</Text>
         <Text>{props.phone}</Text>
     </View>
 )
+
+const styles = StyleSheet.create({
+    row: {
+        padding: 20,
+    }
+})
 
 export default Row; 
